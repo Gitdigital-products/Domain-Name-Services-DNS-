a3121a00c8d22eaefcde3a6e32f931dad2be4cff
Richard Kindler <223166946+RickCreator87@users.noreply.github.com>
2026-02-21 13:51:10 -0700

    Create How to Test It.sh

---
+++ How to Test It.sh
@@ -0,0 +1,7 @@
+How to Test It
+```bash
+dig @127.0.0.1 -p 8053 google.com
+
+```
+What you’ll see:
+Your Python script will print a unique ID and a QDCOUNT of 1. This means Layer 1 (Network) and Layer 2 (Header) are successfully communicating!
